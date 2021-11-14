0928a20de07a27e600fc9b3da87135caa4d4967f
gytukas1979 <79442275+gytukas1979@users.noreply.github.com>
2021-11-14 13:39:51 +0200

    Add project files.

---
+++ AutomatinisTestavimas4NDGV/Page/DropDownPage.cs
@@ -0,0 +1,166 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutomatinisTestavimas4NDGV.Page
+{
+    public class DropDownPage : BasePage
+    {
+        private const string PageAddress = "https://demo.seleniumeasy.com/basic-select-dropdown-demo.html";
+        private const string ResultText = "Day selected :- ";
+        private const string FirstSelectedResultText = "First selected option is : ";
+        private const string OptionsSelectedResultText = "Options selected are : ";
+        private SelectElement DropDown => new SelectElement(Driver.FindElement(By.Id("select-demo")));
+        private IWebElement ResultTextElement => Driver.FindElement(By.CssSelector(".selected-value"));
+        private IWebElement FirstSelectedButton => Driver.FindElement(By.Id("printMe"));
+        private IWebElement GetAllSelectedButton => Driver.FindElement(By.Id("printAll"));
+        private SelectElement MultiDropDown => new SelectElement(Driver.FindElement(By.Id("multi-select")));
+        private IWebElement ResultOfSelectedClick => Driver.FindElement(By.ClassName("getall-selected"));
+        
+        public DropDownPage(IWebDriver webdriver) : base(webdriver)
+        {
+            Driver.Url = PageAddress;
+        }
+
+        public DropDownPage SelectFromDropdownByText(string text)
+        {
+            DropDown.SelectByText(text);
+            return this;
+        }
+
+        public DropDownPage SelectFromDropdownByValue(string text)
+        {
+            DropDown.SelectByValue(text);
+            return this;
+        }
+
+        public DropDownPage SelectFromMultiDropDownByValue(string firstValue, string secondValue)
+        {
+            Actions action = new Actions(Driver);
+            MultiDropDown.SelectByValue(firstValue);
+            action.KeyDown(Keys.Control);
+            MultiDropDown.SelectByValue(secondValue);
+            action.KeyUp(Keys.Control);
+            action.Build().Perform();
+            return this;
+        }
+
+        public DropDownPage ClickFirstSelectedButton()
+        {
+            FirstSelectedButton.Click();
+            return this;
+        }
+
+        public DropDownPage ClickAllSelectedButton()
+        {
+            GetAllSelectedButton.Click();
+            return this;
+        }
+
+        public DropDownPage VerifyResult(string selectedDay)
+        {
+            Assert.IsTrue(ResultTextElement.Text.Equals(ResultText + selectedDay), $"Result is wrong, not {selectedDay}");
+            return this;
+        }
+       
+        public List<string> ListOfTwoStates(string firstState, string secondState)
+        {
+            List<string> listOfStates = new List<string>();
+            listOfStates.Add(firstState);
+            listOfStates.Add(secondState);
+            return listOfStates;
+        }
+
+        public List<string> ListOfThreeStates(string firstState, string secondState, string thirdState)
+        {
+            List<string> listOfStates = new List<string>();
+            listOfStates.Add(firstState);
+            listOfStates.Add(secondState);
+            listOfStates.Add(thirdState);
+            return listOfStates;
+        }
+
+        public List<string> ListOfFourStates(string firstState, string secondState, string thirdState, string fourthState)
+        {
+            List<string> listOfStates = new List<string>();
+            listOfStates.Add(firstState);
+            listOfStates.Add(secondState);
+            listOfStates.Add(thirdState);
+            listOfStates.Add(fourthState);
+            return listOfStates;
+        }
+
+        public DropDownPage SelectFromMultipleDropdownByValueClickFirstSelected(List<string> listOfStates)
+        {
+            MultiDropDown.DeselectAll();
+            Actions action = new Actions(Driver);
+            action.KeyDown(Keys.LeftControl);
+            foreach (string state in listOfStates)
+            {
+                foreach (IWebElement option in MultiDropDown.Options)
+                {
+                    if (state.Equals(option.GetAttribute("value")))
+                    {
+                        action.Click(option);
+                        break;
+                    }
+                }
+            }
+            action.KeyUp(Keys.LeftControl);
+            //action.Build().Perform();
+            action.Click(FirstSelectedButton);
+            action.Build().Perform();
+            return this;
+        }
+
+        public DropDownPage SelectFromMultipleDropdownByValueClickGetAllSelected(List<string> listOfStates)
+        {
+            MultiDropDown.DeselectAll();
+            Actions action = new Actions(Driver);
+            action.KeyDown(Keys.LeftControl);
+            foreach (string state in listOfStates)
+            {
+                foreach (IWebElement option in MultiDropDown.Options)
+                {
+                    if (state.Equals(option.GetAttribute("value")))
+                    {
+                        action.Click(option);
+                        break;
+                    }
+                }
+            }
+            action.KeyUp(Keys.LeftControl);
+            //action.Build().Perform();
+            action.Click(GetAllSelectedButton);
+            action.Build().Perform();
+            return this;
+        }
+
+        private String ReturnStringOfList(List<string> listOfStates)
+        {
+            string expectedResult = string.Join(",", listOfStates.ToArray());
+            return expectedResult;
+        }
+
+        public DropDownPage CheckFirstValue(List<string> listOfStates)
+        {
+            //Console.WriteLine("Rezultatas turi buti: " + ResultOfSelectedClick.Text + " --- o turi buti " + listOfStates[0]);
+            Assert.IsTrue(ResultOfSelectedClick.Text.Equals(FirstSelectedResultText + listOfStates[0]), $"Result is wrong, not {listOfStates[0]}");
+            return this;
+        }
+        public DropDownPage CheckAllSelectedValues(List<string> listOfStates)
+        {
+            //Console.WriteLine("Rezultatas turi buti: " + ResultOfSelectedClick.Text + " --- o turi buti " + ReturnStringOfList(listOfStates));
+            Assert.IsTrue(ResultOfSelectedClick.Text.Contains(ReturnStringOfList(listOfStates)), "The result is incorrect");
+            Assert.IsTrue(ResultOfSelectedClick.Text.Equals(OptionsSelectedResultText + ReturnStringOfList(listOfStates)), $"Result is wrong, not {ReturnStringOfList(listOfStates)}");
+            return this;
+        }
+    }
+}
---
+++ AutomatinisTestavimas4NDGV/Test/DropDownTest.cs
@@ -0,0 +1,135 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatinisTestavimas4NDGV.Page;
+
+namespace AutomatinisTestavimas4NDGV.Test
+{
+    public class DropDownTest
+    {
+        private static DropDownPage _page;
+        public List<string> states = new List<string>() { "Ohio", "New York" };
+        public string tekstas = "labas rytas";
+        
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+            _page = new DropDownPage(driver);
+        }
+
+        [OneTimeTearDown]
+
+        public static void TearDown()
+        {
+            _page.CloseBrowser();
+        }
+
+        [Order(1)]
+        [Test]
+        public void TestDropDown()
+        {
+            _page.SelectFromDropdownByText("Friday")
+                .VerifyResult("Friday");
+        }
+
+        [Order(2)]
+        [Test]
+        public void TestMultiDropDown()
+        {
+            _page.SelectFromMultiDropDownByValue("Ohio", "Florida")
+                .ClickFirstSelectedButton();
+        }
+
+        [Order(3)]
+        [TestCase("Ohio", "Florida")]
+        public void TestTwoStatesSelectedReturnFirstSelected(string firstState, string secondState)
+        {
+            _page.SelectFromMultipleDropdownByValueClickFirstSelected(_page.ListOfTwoStates(firstState, secondState));
+            _page.CheckFirstValue(_page.ListOfTwoStates(firstState, secondState));
+        }
+
+        [Order(4)]
+        [TestCase("Washington", "Texas")]
+        public void TestTwoStatesSelectedReturnGetAllSelected(string firstState, string secondState)
+        {
+            _page.SelectFromMultipleDropdownByValueClickGetAllSelected(_page.ListOfTwoStates(firstState, secondState));
+            _page.CheckAllSelectedValues(_page.ListOfTwoStates(firstState, secondState));
+        }
+
+        [Order(5)]
+        [TestCase("Texas", "Washington", "Ohio")]
+        public void TestThreeStatesSelectedReturnFirstSelected(string firstState, string secondState, string thirdState)
+        {
+            _page.SelectFromMultipleDropdownByValueClickFirstSelected(_page.ListOfThreeStates(firstState, secondState, thirdState));
+            _page.CheckFirstValue(_page.ListOfThreeStates(firstState, secondState, thirdState));
+        }
+
+        [Order(6)]
+        [TestCase("Washington", "New York", "Florida", "Ohio")]
+        public void TestFourStatesSelectedReturnGetAllSelected(string firstState, string secondState, string thirdState, string fourthState)
+        {
+            _page.SelectFromMultipleDropdownByValueClickGetAllSelected(_page.ListOfFourStates(firstState, secondState, thirdState, fourthState));
+            _page.CheckAllSelectedValues(_page.ListOfFourStates(firstState, secondState, thirdState, fourthState));
+        }
+
+        //private static readonly object[] _2states =
+        //{
+        //    new object[] {new List<string> {"California", "Florida"}},   
+        //};
+
+        //[Order(7)]
+        //[Test, TestCaseSource(nameof(_2states))]
+        //public void TestTwoStatesFirstSelected(List<string> list)
+        //{
+        //    _page.SelectFromMultipleDropdownByValueClickFirstSelected(list);
+        //    _page.CheckFirstValue(list);
+        //}
+
+        //private static readonly object[] _3states =
+        //{
+        //    new object[] {new List<string> {"Ohio", "Florida", "California"}},
+        //};
+
+        //[Order(8)]
+        //[Test, TestCaseSource(nameof(_3states))]
+        //public void TestThreeStatesFirstSelected(List<string> list)
+        //{
+        //    _page.SelectFromMultipleDropdownByValueClickFirstSelected(list);
+        //    _page.CheckFirstValue(list);
+        //}
+
+        //private static readonly object[] _2statesForMultipleSelect =
+        //{
+        //    new object[] {new List<string> {"Florida", "California"}},
+        //};
+
+        //[Order(9)]
+        //[Test, TestCaseSource(nameof(_2statesForMultipleSelect))]
+        //public void TestTwoStatesGetAllSelected(List<string> list)
+        //{
+        //    _page.SelectFromMultipleDropdownByValueClickGetAllSelected(list);
+        //    _page.CheckAllSelectedValues(list);
+        //}
+
+        //private static readonly object[] _4statesForMultipleSelect =
+        //{
+        //    new object[] {new List<string> {"Florida", "California", "Texas", "Ohio"}},
+        //};
+
+        //[Order(10)]
+        //[Test, TestCaseSource(nameof(_4statesForMultipleSelect))]
+        //public void TestFourStatesGetAllSelected(List<string> list)
+        //{
+        //    _page.SelectFromMultipleDropdownByValueClickGetAllSelected(list);
+        //    _page.CheckAllSelectedValues(list);
+        //}
+    }
+}
